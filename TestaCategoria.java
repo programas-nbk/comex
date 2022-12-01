3a4583513903daae4f5a950ee4bcb1022847389d
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-01 12:27:16 -0300

    feat(Class): :sparkles: Creating movable objects and printing their data

--- TestaCategoria.java
+++ TestaCategoria.java
@@ -1,10 +1,15 @@
 public class TestaCategoria {
   public static void main(String[] args) {
     Categoria informatica = new Categoria(1, "INFORMÁTICA", "ATIVA");
+    Categoria moveis = new Categoria(2, "MÓVEIS", "INATIVA");
 
     String dadosDaInformatica = String.format("Categoria %s (%d - %s)", informatica.getNome(), informatica.getId(),
         informatica.getStatus());
 
+    String dadosDosMoveis = String.format("Categoria %s (%d - %s)", moveis.getNome(), moveis.getId(),
+        moveis.getStatus());
+
     System.out.println(dadosDaInformatica);
+    System.out.println(dadosDosMoveis);
   }
 }
