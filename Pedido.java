0be8ad6763b7fddbb62b5637c7a78d0bd42095fc
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-01 15:19:45 -0300

    feat(Class): :sparkles: Creating Order class with attributes and their respective getters

---
+++ Pedido.java
@@ -0,0 +1,37 @@
+import java.time.LocalDate;
+
+public class Pedido {
+  int id;
+  LocalDate data;
+  String CPF;
+  Produto produto;
+  int quantidadeVendida;
+
+  public Pedido(int id, LocalDate data, String CPF, Produto produto, int quantidadeVendida) {
+    this.id = id;
+    this.data = data;
+    this.CPF = CPF;
+    this.produto = produto;
+    this.quantidadeVendida = quantidadeVendida;
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public LocalDate getData() {
+    return this.data;
+  }
+
+  public String getCPF() {
+    return this.CPF;
+  }
+
+  public Produto getProduto() {
+    return this.produto;
+  }
+
+  public int getQuantidadeVendida() {
+    return this.quantidadeVendida;
+  }
+}
