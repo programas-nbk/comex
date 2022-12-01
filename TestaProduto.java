514d846973e7b58635db09d56a64e4aaa8b1feb4
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-01 15:07:00 -0300

    feat(Class): :sparkles: Printing product information

--- TestaProduto.java
+++ TestaProduto.java
@@ -8,5 +8,16 @@ public class TestaProduto {
     Produto livro = new Produto(1, "Clean Architecture", 102.90, 2, livros);
 
     Produto monitor = new Produto(3, "Monitor Dell 27", 1889.00, 3, informatica);
+
+    Produto[] listaDeProdutos = { notebook, livro, monitor };
+
+    for (Produto produto : listaDeProdutos) {
+      String dadosDoProduto = String.format(
+          "id: %d, produto: %s, preço unitário: %s, estoque: %s, categoria: %s, valor total em estoque: %s, imposto cobrado: %s",
+          produto.getId(), produto.getNome(), produto.getPrecoUnitario(), produto.getQuantidadeEmEstoque(),
+          produto.getCategoria().getNome(), produto.valorTotalEmEstoque(), produto.calculaImposto());
+
+      System.out.println(dadosDoProduto);
+    }
   }
 }
