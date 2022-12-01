e53cf77256169982fa1c6ec2bebdc1bb97842c0b
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-01 12:24:00 -0300

    feat(Class): :sparkles: Creating computer object and printing its data

--- TestaCategoria.java
+++ TestaCategoria.java
@@ -1,4 +1,10 @@
 public class TestaCategoria {
   public static void main(String[] args) {
+    Categoria informatica = new Categoria(1, "INFORMÁTICA", "ATIVA");
+
+    String dadosDaInformatica = String.format("Categoria %s (%d - %s)", informatica.getNome(), informatica.getId(),
+        informatica.getStatus());
+
+    System.out.println(dadosDaInformatica);
   }
 }
