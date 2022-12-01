c8211124e3ace9abec27704b4047f7b4365569b1
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-01 12:29:42 -0300

    feat(Class): :sparkles: Creating object books and printing its data

--- TestaCategoria.java
+++ TestaCategoria.java
@@ -2,6 +2,7 @@ public class TestaCategoria {
   public static void main(String[] args) {
     Categoria informatica = new Categoria(1, "INFORMÁTICA", "ATIVA");
     Categoria moveis = new Categoria(2, "MÓVEIS", "INATIVA");
+    Categoria livros = new Categoria(3, "LIVROS", "ATIVA");
 
     String dadosDaInformatica = String.format("Categoria %s (%d - %s)", informatica.getNome(), informatica.getId(),
         informatica.getStatus());
@@ -9,7 +10,11 @@ public class TestaCategoria {
     String dadosDosMoveis = String.format("Categoria %s (%d - %s)", moveis.getNome(), moveis.getId(),
         moveis.getStatus());
 
+    String dadosDosLivros = String.format("Categoria %s (%d - %s)", livros.getNome(), livros.getId(),
+        livros.getStatus());
+
     System.out.println(dadosDaInformatica);
     System.out.println(dadosDosMoveis);
+    System.out.println(dadosDosLivros);
   }
 }
