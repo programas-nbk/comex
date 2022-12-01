2e0e6fc86391458a226adbd34d7a0115d4f77a85
Arthur Teixeira <arthurteixeira444@gmail.com>
2022-12-01 12:42:18 -0300

    feat(Class): :sparkles: Creating Product class with attributes and their respective getters and setters

---
+++ Produto.java
@@ -0,0 +1,56 @@
+public class Produto {
+  int id;
+  String nome;
+  String descricao;
+  double precoUnitario;
+  int quantidadeEmEstoque;
+  Categoria categoria;
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return this.nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDescricao() {
+    return this.descricao;
+  }
+
+  public void setDescricao(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public double getPrecoUnitario() {
+    return this.precoUnitario;
+  }
+
+  public void setPrecoUnitario(double precoUnitario) {
+    this.precoUnitario = precoUnitario;
+  }
+
+  public int getQuantidadeEmEstoque() {
+    return this.quantidadeEmEstoque;
+  }
+
+  public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+    this.quantidadeEmEstoque = quantidadeEmEstoque;
+  }
+
+  public Categoria getCategoria() {
+    return this.categoria;
+  }
+
+  public void setCategoria(Categoria categoria) {
+    this.categoria = categoria;
+  }
+}
