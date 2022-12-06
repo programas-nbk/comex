be111702d06f9dfe88f9fd0013b7774732757536
John Soares <john.soares@john.soares>
2022-12-06 01:12:43 -0300

    Correcao geral em todas as classes, incluindo: ajuste de atributo,testes com  array de objetos e etc.

--- TestaPedido.java
+++ TestaPedido.java
@@ -5,7 +5,9 @@ public class TestaPedido {
         obj[0] = new Categoria("INFORMÁTICA","ATIVA");
         obj[1] = new Categoria("LIVROS","ATIVA");
 
-        Cliente cliente1 = new Cliente("John", "Wesley", "000.000.000.00", "3222222222", "Rua ABCD", "1234", "", "BAIRRO", "CIDADE", "ESTADO");
+        Cliente cliente1 = new Cliente("John", "Wesley", "000.000.000.00",
+                "3222222222", "Rua ABCD", "1234", "",
+                "BAIRRO", "CIDADE", "ESTADO");
 
         Produto produto1 = new Produto("Notebook", "Samsung",3523.0, 1, obj[0]);
         Produto produto2 = new Produto("Livro","Clean Architecture", 102.90, 2, obj[1]);
